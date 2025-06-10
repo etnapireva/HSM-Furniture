fcbe01ed252bd179bd4ed0b649c1616f4489c103
limonmorina <149487995+limonmorina@users.noreply.github.com>
2025-06-10 14:42:34 +0100

    .

---
+++ frontend/craco.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        util: require.resolve('util/'),
+        zlib: require.resolve('browserify-zlib'),
+        stream: require.resolve('stream-browserify'),
+        url: require.resolve('url/'),
+        crypto: require.resolve('crypto-browserify'),
+        assert: require.resolve('assert/'),
+      };
+      return webpackConfig;
+    },
+  },
+};
